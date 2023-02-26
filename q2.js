40c2082504731d196b5ab5dd3d07add946ee9fde
zohaibsadiq <abdullahjavaid899@gmail.com>
2023-02-26 08:45:56 +0500

    Second question

---
+++ q2.js
@@ -0,0 +1,8 @@
+/*
+Q2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your
+message should be simple, such as, “Hello Eric,
+would you like to learn some Python today?”
+
+*/
+var person_name = 'talha';
+console.log(Aslam o alaikum ${person_name}  will you attendent the next class?);
